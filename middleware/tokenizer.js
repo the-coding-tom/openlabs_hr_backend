f723d5457d6d528a062b314a34ec08f6e2e19e39
Thomas Kensah <tom.kensah@gmail.com>
2022-06-03 08:39:59 -0700

    cleaned up middleware function

--- middleware/tokenizer.js
+++ middleware/tokenizer.js
@@ -3,15 +3,16 @@ import jwt from "jsonwebtoken";
 export const verifyToken = (request, response, next) => {
   try {
     const bearerToken = request.header("Authorization").replace("Bearer ", "");
-    console.log(bearerToken);
     const decoded = jwt.verify(bearerToken, process.env.JWT_Secret);
-    console.log(decoded);
 
     //Add the ID to the request body
     request.body.userId = decoded.userId;
+
     next();
   } catch (exception) {
-    response.status(401).json({});
+    response
+      .status(401)
+      .json({ data: {}, errors: [], message: "Token is invalid" });
   }
 };
 
