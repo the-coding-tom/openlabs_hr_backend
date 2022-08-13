ccc949860b0f79e67a408d1fc25394e662ff28d2
Thomas Kensah <tom.kensah@gmail.com>
2022-08-13 06:07:44 -0700

    installed and configured axios

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import express from "express";
 import router from "./routes.js";
 import cors from "cors";
 import database from "./database.config.js";
+import axios from "axios";
 
 //Connect to database
 database.connect();
@@ -11,9 +12,31 @@ const app = express();
 
 app.use(cors());
 app.use("/images", express.static("public"));
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false })); // https://www.monkeychops.com
 app.use(express.json());
 app.use(router);
 
 const myServer = http.createServer(app);
 myServer.listen(process.env.PORT || 9000);
+
+let email = "tom.kensah@gmail.com";
+let password = "password";
+
+async function makeRequest() {
+  try {
+    // code to try...
+    const response = await axios.post("http://localhost:9090/auth/login", {
+      email,
+      password,
+    });
+
+    console.log(
+      `This is the data provided: ${JSON.stringify(response.data.data)}`
+    );
+  } catch (error) {
+    // what should I do about the error...
+    console.log(error);
+  }
+}
+
+makeRequest();
